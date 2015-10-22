936607ec515f396bf3592b56aa47f120760b2152
Felipe Baccin <felipebaccincvel@gmail.com>
2015-10-22 14:42:11 -0200

    criada a classe professor com os atributos

---
+++ ExJMS/src/main/java/br/com/feltex/academicnet/entidade/Professor.java
@@ -0,0 +1,26 @@
+package br.com.feltex.academicnet.entidade;
+
+import java.io.Serializable;
+
+public class Professor implements Serializable {
+
+	private long matricula;
+	private String nome;
+	
+	public long getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(long matricula) {
+		this.matricula = matricula;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
+	
+}
